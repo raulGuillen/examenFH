abbb355d674bba6825e1f5b54ed7841fa33ee0bb
raulguillen11 <raulguillen11@gmail.com>
2025-10-03 21:19:59 +0000

    Fix: Configurar el escaneo de componentes y los repositorios JPA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.demo", "com.example.controller"})
+@ComponentScan(basePackages = {"com.example.demo", "com.example.controller", "com.example.service", "com.example.repository"})
+@EnableJpaRepositories("com.example.repository")
+@EntityScan("com.example.entity")
 public class DemoApplication {
 
   @Value("${NAME:World}")
